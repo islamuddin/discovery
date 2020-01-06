8ee5f209b1423b1a06ecb933c834d2a64559175b
ID KHAN <uddinislam46@gmail.com>
2020-01-06 12:11:09 +0500

    class assignment

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -59,27 +59,3 @@ fn led_control(led_index:usize,led_status:bool,half_period:u16,delay:&mut Delay,
         led_control(led_index+1,led_status,half_period,delay,leds);
     }
 }
-
-
-// #![deny(unsafe_code)]
-// #![no_main]
-// #![no_std]
-
-// use aux5::{entry, prelude::*, Delay, Leds};
-
-// #[entry]
-// fn main() -> ! {
-//     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
-
-//     let ms = 50_u8;
-//     loop {
-//         for curr in 0..8 {
-//             let next = (curr + 1) % 8;
-
-//             leds[next].on();
-//             delay.delay_ms(ms);
-//             leds[curr].off();
-//             delay.delay_ms(ms);
-//         }
-//     }
-// }

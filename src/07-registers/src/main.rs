db4f2f08d0b3282b4cae8caa6e0ce1e92d9483c4
ID KHAN <uddinislam46@gmail.com>
2020-01-19 08:32:10 +0500

    07-registers assignment work

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -4,25 +4,25 @@
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
+static PE8_ON: u32 = 8;
+static PE8_OFF: u32 = PE8_ON+16;
+
+static PE9_ON: u32 = 9;
+static PE9_OFF: u32 = PE9_ON+16;
+
+
 #[entry]
 fn main() -> ! {
-    aux7::init();
-
+    aux7::init();    
+    const GPIOE_BSRR: u32 = 0x48001018; // A magic address!
     unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+        let led_switch=|a:u32| *(GPIOE_BSRR as *mut u32)= 1 << a;
 
-        // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+        led_switch(PE9_ON);
+        led_switch(PE8_ON);
 
-        // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
-
-        // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
-
-        // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+        led_switch(PE9_OFF);
+        led_switch(PE8_OFF);
     }
 
     loop {}
